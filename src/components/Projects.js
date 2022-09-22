260fd781a17cbe05325f27b33bd3329e0f7ee577
Megan McBride <meganambermcbride@gmail.com>
2022-09-22 11:07:11 -0400

    Fix bottom margin projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -24,6 +24,7 @@ function Projects() {
           autoPlay={true}
           emulateTouch={true}
           swipeable={true}
+          // infiniteLoop={true}
    
         >
           <div className="item">
