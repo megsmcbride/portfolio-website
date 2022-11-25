842e4e2ff341cc5b0330b5de8697a3d6dbddd321
Megan McBride <meganambermcbride@gmail.com>
2022-11-25 09:57:53 -0500

    Move social icons to contact section

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,10 @@
 import React, { useRef, useState } from "react";
 import emailjs from '@emailjs/browser';
+import { SocialIcon } from "react-social-icons";
 
 function Contact() {
+  const [linkedInIconColor, setLinkedInIconColor] = useState("#de5499");
+  const [githubIconColor, setGithubIconColor] = useState("#de5499");
   const [ emailSent, setEmailSent ] = useState(false)
   const form = useRef();
 
@@ -40,6 +43,38 @@ function Contact() {
           )
         }
         </form>
+
+        <div className="website-links">
+              <SocialIcon
+                onMouseEnter={() => setGithubIconColor("#e99f4c")}
+                onMouseLeave={() => setGithubIconColor("#de5499")}
+                url="https://github.com/megsmcbride"
+                target="_blank"
+                bgColor={githubIconColor}
+                fgColor="#f2ebe9"
+                style={{
+                  height: "2.4vw",
+                  width: "2.4vw",
+                  maxHeight: "40px",
+                  maxWidth: "40px",
+                }}
+              />
+              <SocialIcon
+                onMouseEnter={() => setLinkedInIconColor("#e99f4c")}
+                onMouseLeave={() => setLinkedInIconColor("#de5499")}
+                url="https://www.linkedin.com/in/megsmcbride"
+                target="_blank"
+                bgColor={linkedInIconColor}
+                fgColor="#f2ebe9"
+                style={{
+                  height: "2.4vw",
+                  maxHeight: "40px",
+                  maxWidth: "40px",
+                  width: "2.4vw",
+                  marginLeft: "1.5vw",
+                }}
+              />
+            </div>
       </div>
     </div>
     </>
